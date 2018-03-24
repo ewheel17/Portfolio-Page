66c079830e59f5b875aa084c7871a345a35bb085
Max <u1110071@utah.edu>
2018-03-24 13:13:00 -0600

    Full Firebase update

--- Portfolio-Page/assets/javascript/app.js
+++ Portfolio-Page/assets/javascript/app.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCtBuyV-RTxNpqbC99Qp2HbXBE0JYfVrnk",
+    authDomain: "portfolio-site-82e35.firebaseapp.com",
+    databaseURL: "https://portfolio-site-82e35.firebaseio.com",
+    projectId: "portfolio-site-82e35",
+    storageBucket: "portfolio-site-82e35.appspot.com",
+    messagingSenderId: "266605929586"
+  };
+  firebase.initializeApp(config);
+
+
 $("#navTop").click(function() {
     $('html, body').animate({
         scrollTop: $("#page-top").offset().top - 50
